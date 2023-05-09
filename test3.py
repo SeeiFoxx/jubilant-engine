f6e5023c1bbb0c09799db7e5780ac31b18618459
Pingasmaster <93002614+Pingasmaster@users.noreply.github.com>
2023-05-09 11:16:14 +0200

    Update test3.py

--- test3.py
+++ test3.py
@@ -187,7 +187,7 @@ def draw_chart(screen, chart, color):
 # Définition de la fonction pour dessiner l'écran du joueur
 def draw_player_screen(screen):
     # affiche le menu de gauche avec les portefeuilles
-    pygame.draw.rect(screen, blanc, [fenetre[1] - 300, 0, 300, fenetre[0]])
+    pygame.draw.rect(screen, blanc, [fenetre[1] - 300, 0, fenetre[1], fenetre[0]])
     text1 = police.render('Solde ...', True, noir)
     screen.blit(text1, [fenetre[1] - 280, 25])
     text2 = police.render('Pycoin | Perso | Emprunté', True, noir)
