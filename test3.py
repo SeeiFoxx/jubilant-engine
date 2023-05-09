69386c6cf961f041048906a056d8c8555ad78a59
Pingasmaster <93002614+Pingasmaster@users.noreply.github.com>
2023-05-09 11:43:20 +0200

    Update test3.py

--- test3.py
+++ test3.py
@@ -243,7 +243,7 @@ def main_menu_init_temp():
 # Initialise le menu principal
 def main_menu_init():
     # Importe les variables
-    global noir, blanc, or_, police, fenetre, liste_monnaies, current_crypto, screen, chart, clock, continuer
+    global noir, blanc, or_, police, fenetre, liste_monnaies, current_crypto, screen, chart, clock, continuer, police_bouton
     global Dollar, Euro, Bitcoin, Ethereum, Litecoin, Dogecoin, Shiba, IOTA, Ergo, Isotopec 
     # Définition de la police de caractères
     police = pygame.font.Font('police.ttf', 25)
@@ -274,7 +274,7 @@ def main_menu_init():
 
 # Définition de la fonction principale
 def main_menu():
-    global noir, blanc, or_, police, fenetre, liste_monnaies, current_crypto, screen, chart, clock, continuer
+    global noir, blanc, or_, police, fenetre, liste_monnaies, current_crypto, screen, chart, clock, continuer, police_bouton
     global Dollar, Euro, Bitcoin, Ethereum, Litecoin, Dogecoin, Shiba, IOTA, Ergo, Isotopec
     # Boucle du jeu
     while continuer:
